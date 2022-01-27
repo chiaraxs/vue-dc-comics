7ac5d27b58ae06a1b03ecc1f7e4047d373aee235
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-01-27 14:58:30 +0100

    Structure completed

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
+    
+    <header-box />
+    <hero-box />
+    <main-content />
+    <section-shop />
+    <footer-box />
+    <section-contacts />
 
   </div>
 </template>
 
 <script>
-
+import HeaderBox from './components/HeaderBox.vue'
+import HeroBox from './components/HeroBox.vue'
+import MainContent from './components/MainContent.vue'
+import SectionShop from './components/SectionShop.vue'
+import FooterBox from './components/FooterBox.vue'
+import SectionContacts from './components/SectionContacts.vue'
 
 export default {
   name: 'App',
   components: {
+    HeaderBox,
+    HeroBox,
+    MainContent,
+    SectionShop,
+    FooterBox,
+    SectionContacts,
   }
 }
 </script>
 
 <style lang="scss">
+@import './style/common.scss';
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 
 </style>
