b6c44f4a6a8e4330444200f3affec510981f5fae
Chiara Cardinale <chiaracardinale92@gmail.com>
2022-01-27 19:01:01 +0100

    Added: SectionContacs (temporary style)

--- src/components/SectionContacts.vue
+++ src/components/SectionContacts.vue
@@ -1,5 +1,20 @@
 <template>
-    <div></div>
+    <section>
+        <div class="contacts">
+            <div class="sign-up">
+                <button>SIGN-UP NOW!</button>
+            </div>
+
+                <div class="icons-zone">
+                    <span>FOLLOW US</span>
+                    <img src="../assets/footer-facebook.png" alt="">
+                    <img src="../assets/footer-twitter.png" alt="">
+                    <img src="../assets/footer-youtube.png" alt="">
+                    <img src="../assets/footer-pinterest.png" alt="">
+                    <img src="../assets/footer-periscope.png" alt="">
+                </div>
+        </div>
+    </section>
 </template>
 
 <script>
@@ -8,6 +23,29 @@ export default {
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+.contacts{
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #303030;
+
+    .sign-up{
+        margin-left: 100px;
+        }
+    
+
+    .icons-zone{
+        text-align: center;
+
+        margin-right: 200px;
+        img {
+           padding: 10px;
+        }
+    }
+}
+
 
 </style>
